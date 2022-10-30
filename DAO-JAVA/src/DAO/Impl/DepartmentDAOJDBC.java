8c55d927751e1a78e5278f568a6c6d4eedf00210
Gabriel <prismaticnetwork12@gmail.com>
2022-10-30 11:54:01 -0300

    feat: adding sql query to DepartmentDAOJDBC

--- DAO-JAVA/src/DAO/Impl/DepartmentDAOJDBC.java
+++ DAO-JAVA/src/DAO/Impl/DepartmentDAOJDBC.java
@@ -1,20 +1,38 @@
 package DAO.Impl;
 
 import DAO.DepartmentDAO;
+import db.DB;
+import db.DbException;
 import models.Department;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class DepartmentDAOJDBC implements DepartmentDAO {
   private Connection connection;
+  private static PreparedStatement statement = null;
+  private static ResultSet resultSet = null;
   public DepartmentDAOJDBC(Connection connection) {
     this.connection = connection;
   }
 
   @Override
   public void insertDepartment(Department object) {
-
+    try {
+      statement = connection.prepareStatement("INSERT INTO department VALUES (?,?)");
+      statement.setInt(1,object.getId());
+      statement.setString(2, object.getName());
+      statement.executeQuery();
+    } catch (SQLException e) {
+      throw new DbException(e.getMessage());
+    } finally {
+      DB.closeConnection();
+      DB.closeStatement(statement);
+    }
   }
 
   @Override
@@ -29,11 +47,46 @@ public class DepartmentDAOJDBC implements DepartmentDAO {
 
   @Override
   public Department findById(Integer id) {
-    return null;
+    try {
+      statement = connection.prepareStatement("SELECT * FROM department WHERE id = ?");
+      statement.setInt(1, id);
+      resultSet = statement.executeQuery();
+      if (resultSet.next()) {
+        Department department = implementDepartment(resultSet);
+        return department;
+      }
+      return null;
+    } catch (SQLException e) {
+      throw new DbException(e.getMessage());
+    } finally {
+      DB.closeConnection();
+      DB.closeStatement(statement);
+    }
   }
 
   @Override
   public List<Department> findAll() {
-    return null;
+    List<Department> departments = new ArrayList<>();
+    try {
+      statement = connection.prepareStatement("SELECT * FROM department");
+      resultSet = statement.executeQuery();
+      while(resultSet.next()) {
+        departments.add(implementDepartment(resultSet));
+      }
+      return departments;
+    } catch (SQLException e) {
+      throw new DbException(e.getMessage());
+    } finally {
+      DB.closeConnection();
+      DB.closeStatement(statement);
+      DB.closeResultSet(resultSet);
+    }
+  }
+
+  private Department implementDepartment(ResultSet resultSet) throws SQLException {
+    Department department = new Department();
+    department.setId(resultSet.getInt("Id"));
+    department.setName(resultSet.getString("Name"));
+    return department;
   }
 }
